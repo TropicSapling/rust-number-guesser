ac0bb01c2b4c0379870f94c54379f720a8b4514c
TropicSapling <tropical.productions.scratch@gmail.com>
2018-04-17 19:05:01 +0200

    Removed unnecessary files

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-WebAssembly.instantiateStreaming(fetch('../wasm/target/wasm32-unknown-unknown/release/wasm.wasm'), importObject)
+WebAssembly.instantiateStreaming(fetch('../wasm/wasm.wasm'), importObject)
 .then(wasm_module => {
 	alert("2 + 1 = " + wasm_module.instance.exports.add_one(2));
 });
