91fdcaa153d65621cddfe081f906dc2f21b19f5a
TropicSapling <tropical.productions.scratch@gmail.com>
2018-04-17 20:37:06 +0200

    Attempting to port number-guesser to wasm

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
+var memory;
+
+function print_js(s, l) {
+	// Create a buffer starting at the reference to the exported string
+	const stringBuffer = new Uint8Array(memory.buffer, s, l);
+	
+	// Create a string from this buffer
+	let str = '';
+	for let i = 0; i < stringBuffer.length; i++) {
+		str += String.fromCharCode(stringBuffer[i]);
+	}
+	
+	// Print string
+	console.log(str);
+}
+
 WebAssembly.instantiateStreaming(fetch('wasm/wasm.wasm'))
 .then(wasm_module => {
-	alert("2 + 1 = " + wasm_module.instance.exports.add_one(2));
+	memory = wasm_module.instance.exports.memory;
+	
+	wasm_module.instance.exports.run();
 });
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,215 @@
+extern crate rand;
+
+use rand::{Rng, thread_rng};
+use std::num::Wrapping;
+
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+#[derive(Debug, Clone, Copy)]
+struct Node {
+    op: Operator,
+    val: Wrapping<i64>,
+    mut_rate: f32,
+    mut_rate2: f32
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operator {
+    Add,
+    Sub,
+    Mul,
+    Div
+}
+
+extern {
+	fn print_js(s: *mut c_char, l: usize);
+}
+
+fn print(s: String) {
+	let length = s.len();
+	
+	unsafe {
+		print_js(CString::new(s).unwrap().into_raw(), length);
+	}
+}
+
 #[no_mangle]
-pub extern fn add_one(a: u32) -> u32 {
-    a + 1
+pub extern fn run() {
+    let mut rng = thread_rng();
+    let mut ai = std::iter::repeat(vec![]).take(256).collect::<Vec<_>>();
+    for nodes in ai.iter_mut() {
+        for _ in 0..rng.gen_range(2, 8) {
+            let random: f32 = rng.gen();
+            nodes.push(Node {
+                op: if random < 0.25 {
+                    Operator::Add
+                } else if random < 0.5 {
+                    Operator::Sub
+                } else if random < 0.75 {
+                    Operator::Mul
+                } else {
+                    Operator::Div
+                },
+                
+                val: Wrapping(rng.gen_range(-2048, 2048)),
+                mut_rate: rng.gen(),
+                mut_rate2: rng.gen()
+            });
+        }
+    }
+    
+    let mut best = None;
+    let mut best_of_the_best = 0;
+    for _ in 0..256 {
+        let mut closest = None;
+        let mut best_ai = None;
+        
+        for (i, nodes) in ai.iter().enumerate() {
+            let mut res = Wrapping(rng.gen_range(-2048, 2048));
+            for node in nodes.iter() {
+                match node.op {
+                    Operator::Add => res += node.val,
+                    Operator::Sub => res -= node.val,
+                    Operator::Mul => res *= node.val,
+                    Operator::Div => if node.val != Wrapping(0) {
+                        res /= node.val
+                    }
+                }
+            }
+            
+            let diff = if res > Wrapping(1337) {
+                res - Wrapping(1337)
+            } else {
+                Wrapping(1337) - res
+            };
+            
+            match closest {
+                None => {
+                    closest = Some(diff);
+                    best_ai = Some(i);
+                },
+                
+                Some(ref mut closest) => if diff < *closest || (diff == *closest && nodes.len() < ai[best_ai.unwrap()].len()) {
+                    *closest = diff;
+                    best_ai = Some(i);
+                }
+            }
+        }
+        
+        match best {
+            None => best = Some(closest.unwrap()),
+            Some(ref mut best) => if closest.unwrap() < *best {
+                *best = closest.unwrap();
+            }
+        }
+        
+        print(format!("Output: {}", closest.unwrap()));
+        
+        let mut i = 0;
+        while i < ai.len() {
+            if i != best_ai.unwrap() {
+                let mut j = 0;
+                while j < ai[i].len() {
+                    if rng.gen::<f32>() < ai[i][j].mut_rate {
+                        let best = ai[best_ai.unwrap()].clone();
+                        let random = rng.gen_range(0, best.len());
+                        
+                        ai[i][j] = best[random];
+                        
+                        if j < best.len() {
+                            let random: f32 = rng.gen();
+                            ai[i].push(Node {
+                                op: if random < 0.25 {
+                                    Operator::Add
+                                } else if random < 0.5 {
+                                    Operator::Sub
+                                } else if random < 0.75 {
+                                    Operator::Mul
+                                } else {
+                                    Operator::Div
+                                },
+                                
+                                val: Wrapping(rng.gen_range(-2048, 2048)),
+                                mut_rate: rng.gen(),
+                                mut_rate2: rng.gen()
+                            });
+                        } else if j > 0 {
+                            ai[i].pop();
+                        }
+                    } else if rng.gen::<f32>() < ai[i][j].mut_rate2 {
+                        let random: f32 = rng.gen();
+                        ai[i][j].op = if random < 0.25 {
+                            Operator::Add
+                        } else if random < 0.5 {
+                            Operator::Sub
+                        } else if random < 0.75 {
+                            Operator::Mul
+                        } else {
+                            Operator::Div
+                        };
+                        
+                        ai[i][j].val = Wrapping(rng.gen_range(-2048, 2048));
+                        ai[i][j].mut_rate = rng.gen();
+                        ai[i][j].mut_rate2 = rng.gen();
+                        
+                        if rng.gen() {
+                            let random: f32 = rng.gen();
+                            ai[i].push(Node {
+                                op: if random < 0.25 {
+                                    Operator::Add
+                                } else if random < 0.5 {
+                                    Operator::Sub
+                                } else if random < 0.75 {
+                                    Operator::Mul
+                                } else {
+                                    Operator::Div
+                                },
+                                
+                                val: Wrapping(rng.gen_range(-2048, 2048)),
+                                mut_rate: rng.gen(),
+                                mut_rate2: rng.gen()
+                            });
+                        } else if j > 0 {
+                            ai[i].pop();
+                        }
+                    }
+                    
+                    j += 1;
+                }
+            }
+            
+            i += 1;
+        }
+        
+        best_of_the_best = best_ai.unwrap();
+    }
+    
+    print(format!("\nFinal Output: {}", best.unwrap()));
+    
+    let mut res = Wrapping(rng.gen_range(-2048, 2048));
+    for node in ai[best_of_the_best].iter() {
+        match node.op {
+            Operator::Add => {
+                let s = format!("{} + {} = ", res, node.val);
+                res += node.val;
+                print(s + &format!("{}", res));
+            },
+            Operator::Sub => {
+                let s = format!("{} - {} = ", res, node.val);
+                res -= node.val;
+                print(s + &format!("{}", res));
+            },
+            Operator::Mul => {
+                let s = format!("{} * {} = ", res, node.val);
+                res *= node.val;
+                print(s + &format!("{}", res));
+            },
+            Operator::Div => if node.val != Wrapping(0) {
+                let s = format!("{} / {} = ", res, node.val);
+                res /= node.val;
+                print(s + &format!("{}", res));
+            }
+        }
+    }
 }
